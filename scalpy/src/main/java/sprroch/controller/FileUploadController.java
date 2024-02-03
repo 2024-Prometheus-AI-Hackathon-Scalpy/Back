716408ae3f99538431a3617ecc17a4bf9b49d8e1
Yejin Kim <150774446+yeejkim@users.noreply.github.com>
2024-02-03 09:19:21 +0900

    API 구현

---
+++ scalpy/src/main/java/sprroch/controller/FileUploadController.java
@@ -0,0 +1,52 @@
+package sprroch.controller;
+
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import java.io.IOException;
+
+
+@RestController
+@RequestMapping("/upload")
+public class FileUploadController {
+
+    private final AmazonS3 amazonS3;
+
+    @Value("${cloud.aws.s3.bucket}")
+    private String bucket;
+
+    @Autowired
+    public FileUploadController(AmazonS3 amazonS3) {
+        this.amazonS3 = amazonS3;
+    }
+
+    @PostMapping
+    public ResponseEntity<String> uploadFile(@RequestParam("file") MultipartFile file) {
+        if (file.isEmpty()) {
+            System.out.println("Debug message: This is a log message for debugging.");
+            return ResponseEntity.badRequest().body("File is empty or not provided");
+        }
+
+        try {
+            String fileName = "scalpy.jpg"; // 고정된 파일 이름
+
+            String fileUrl = "https://" + bucket + ".s3.amazonaws.com/" + fileName;
+            ObjectMetadata metadata = new ObjectMetadata();
+            metadata.setContentType(file.getContentType());
+            metadata.setContentLength(file.getSize());
+            amazonS3.putObject(bucket, fileName, file.getInputStream(), metadata);
+            System.out.println("Debug message: This is a log message for debugging.");
+
+            return ResponseEntity.ok(fileUrl);
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.out.println("Debug message: This is a log message for debugging.");
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error processing file upload");
+        }
+    }
+}
---
+++ scalpy/src/test/java/sprroch/scalpy/ScalpyApplicationTests.java
@@ -0,0 +1,13 @@
+package sprroch.scalpy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScalpyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
